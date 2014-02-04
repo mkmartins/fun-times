beefaa2944ebf842eaeb9a9813aa0dcae4a7db36
Marcos Martins <marcos.martins2307@gmail.com>
2014-02-04 09:26:41 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunTimes::Application.config.secret_token = 'e31a208d107384d0fd46e5136aaad9f4cac70eab0dd25e3236c182e88b55b42eb213919f332c0523646421b1f8688d9efd1b376be7e84d74239470e0dc5bc8ff'
